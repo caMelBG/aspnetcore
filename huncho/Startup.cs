07739322bdd8a1ccbe6d77c10dff3b2098a14de7
Tihomir Ivanov <tihomir.ivanov1564@gmail.com>
2019-05-12 11:43:36 +0300

    Adding data seeder

--- huncho/Startup.cs
+++ huncho/Startup.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using huncho.Data;
+using huncho.Data.Seeders;
+using huncho.Extensions;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using huncho.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using huncho.Data.Repositories;
-using huncho.Extensions;
 
 namespace huncho
 {
@@ -74,6 +69,8 @@ namespace huncho
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            SeedData.EnsurePopulated(app);
         }
     }
 }
